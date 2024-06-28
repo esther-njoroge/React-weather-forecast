190024ee5e99f736c5a4abfbbd665d0c186d9eb2
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-06-28 19:33:11 +0300

    add files

---
+++ src/Weather/index.js
@@ -0,0 +1,54 @@
+
+
+const Weather = () =>{
+    return(
+        <div>
+            <div>
+                <nav>
+                <h3>Rayn</h3>
+                <input placeholder="Enter city  name..."/>
+                </nav>
+            </div>
+
+            <div>
+                <img/>
+            </div>
+
+            <div>
+                <div>
+                <img/>
+                <p>Humidity</p>
+                <p>39%</p>
+
+                <img/>
+                <p>Sunset</p>
+                <p>7:50 pm</p>
+    
+                </div>
+
+                <div>
+                <img/>
+                <p>UV Index</p>
+                <p>0 0f 10</p>
+
+                <img/>
+                <p>Sunset</p>
+                <p>6:35pm</p>
+                </div>
+
+                <div>
+                    <p>Monthly Rainfall</p>
+                    <p>45mm</p>
+                    <p>This Year</p>
+                    <p>+17%</p>
+
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Weather;
