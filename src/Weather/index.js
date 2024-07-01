86aa19fb63e734c1247cd2aa5e1b3f66924a7f4b
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-07-01 12:23:54 +0300

    commit changes

--- src/Weather/index.js
+++ src/Weather/index.js
@@ -1,46 +1,51 @@
-
+import './index.css'
 
 const Weather = () =>{
     return(
         <div>
-            <div>
+            <div className="navbar">
                 <nav>
-                <h3>Rayn</h3>
-                <input placeholder="Enter city  name..."/>
+                <h3 className="heading">Rayn</h3>
+                <input placeholder="Enter city  name..." className="search"/>
                 </nav>
             </div>
 
             <div>
-                <img/>
+                <img src="/images/landing.png" className='landing-page'/>
             </div>
 
             <div>
                 <div>
-                <img/>
+                <img src="/images/humidity.jpeg"/>
                 <p>Humidity</p>
                 <p>39%</p>
 
-                <img/>
+                <img src="/images/sunset3.png"/>
                 <p>Sunset</p>
                 <p>7:50 pm</p>
     
                 </div>
 
                 <div>
-                <img/>
+                <img src="/images/sun2.webp"/>
                 <p>UV Index</p>
                 <p>0 0f 10</p>
 
-                <img/>
-                <p>Sunset</p>
+                <img src="/images/sunrise3.png"/>
+                <p>Sunrise</p>
                 <p>6:35pm</p>
                 </div>
 
+                <div>
                 <div>
                     <p>Monthly Rainfall</p>
                     <p>45mm</p>
                     <p>This Year</p>
                     <p>+17%</p>
+                    </div>
+                    <div>
+                        <img src="/images/temperature.jpeg"/>
+                    </div>
 
                 </div>
             </div>
